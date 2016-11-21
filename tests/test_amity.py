fdf015d6aa7520c26700850c9123ecf0a3275480
andela-akhenda <joseph.akhenda@andela.com>
2016-11-21 23:00:30 +0300

    Wrote most of the test fuctions I expect to implement

--- tests/test_amity.py
+++ tests/test_amity.py
@@ -1,34 +1,63 @@
-# -*- coding: utf-8 -*-
-
-from .context import sample
-
 import unittest
 
-
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
-
-    def test_absolute_truth_and_meaning(self):
-        assert True
+from amity.amity import Amity
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestAmity(unittest.TestCase):
 
+    def setUp(self):
+        self.amity = Amity()
 
+    def test_create_room(self):
+        pass
 
-# -*- coding: utf-8 -*-
+    def test_add_person(self):
+        pass
 
-from .context import sample
+    def test_if_a_person_is_fellow_or_staff(self):
+        pass
 
-import unittest
+    def test_create_multiple_rooms(self):
+        pass
 
+    def test_cannot_add_duplicate_room(self):
+        pass
 
-class AdvancedTestSuite(unittest.TestCase):
-    """Advanced test cases."""
+    def test_occupants_increment_on_allocation(self):
+        pass
 
-    def test_thoughts(self):
-        sample.hmm()
+    def test_add_to_fully_occupied_rooms(self):
+        pass
+
+    def test_empty_rooms(self):
+        pass
+
+    def test_response_on_no_rooms(self):
+        pass
+
+    def test_occupants_after_relallocation(self):
+        pass
+
+    def test_staff_cannot_be_allocated_living_space(self):
+        pass
+
+    def test_print_allocations(self):
+        pass
+
+    def test_print_unallocated(self):
+        pass
+
+    def test_print_room(self):
+        pass
+
+    def test_load_people_from_file(self):
+        pass
+
+    def test_save_state(self):
+        pass
+
+    def test_load_state(self):
+        pass
 
 
 if __name__ == '__main__':
