b96468445dc599b445c30572a8b33737eebe53c9
andela-akhenda <joseph.akhenda@andela.com>
2016-11-22 18:01:20 +0300

    Added Person and Room Classes structures

--- amity/person.py
+++ amity/person.py
@@ -1,10 +1,27 @@
 class Person(object):
-    pass
+
+    def __init__(self, uuid, name, role):
+        self.allocated_rooms = []
+
+    def print_person(self):
+        pass
 
 
 class Fellow(Person):
-    pass
+    number_of_fellows = 0
+
+    def __init__(self, name):
+        Person.__init__("f" + str(self.number_of_fellows + 1), name, "Fellow")
+
+    def is_allocated_room(self):
+        pass
 
 
 class Staff(Person):
-    pass
+    number_of_staff = 0
+
+    def __init__(self, name):
+        Person.__init__("s" + str(self.number_of_staff + 1), name, "Staff")
+
+    def is_allocated_room(self):
+        pass
--- amity/room.py
+++ amity/room.py
@@ -1,10 +1,27 @@
 class Room(object):
-    pass
+
+    def __init__(self, name, type):
+        self.total_persons = 0
+        self.allocated_persons = []
+
+    def add_person(self, uuid):
+        pass
+
+    def remove_person(self, uuid):
+        pass
 
 
 class Office(Room):
-    pass
+    number_of_offices = 0
+
+    def __init__(self, name):
+        Room.__init__(name, "Office")
+        self.max_persons = 6
 
 
 class LivingSpace(Room):
-    pass
+    number_of_living_spaces = 0
+
+    def __init__(self, name):
+        Room.__init__(name, "Living Space")
+        self.max_persons = 4
