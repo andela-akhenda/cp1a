0b1bab6a627991605b71ce338569d525cf01fa11
andela-akhenda <joseph.akhenda@andela.com>
2016-11-22 22:12:38 +0300

    Added docstrings

--- amity/person.py
+++ amity/person.py
@@ -1,13 +1,31 @@
 class Person(object):
+    ''' Person Class
 
+        This is the Super Class that handles the creation of Persons
+        in the Amity System. It relies heavily on the information it
+        gets from it's Child Classes.
+    '''
     def __init__(self, uuid, name, role):
+        self.uuid = uuid
+        self.name = name
+        self.role = role
         self.allocated_rooms = []
 
     def print_person(self):
+        ''' This method is responsible for printing all the
+            Person's details.
+        '''
         pass
 
 
 class Fellow(Person):
+    ''' Fellow Class
+
+        This is one of the Child Classes that inherits from the Person
+        Super Class. It passes information to the Parent Constructor
+        so as to dictate how the Person is created. It also handles
+        other responsibilities related to a Fellow.
+    '''
     number_of_fellows = 0
 
     def __init__(self, name):
@@ -18,6 +36,13 @@ class Fellow(Person):
 
 
 class Staff(Person):
+    ''' Staff Class
+
+        This is one of the Child Classes that inherits from the Person
+        Super Class. It passes information to the Parent Constructor
+        so as to dictate how the Person is created. It also handles
+        other responsibilities related to a Staff.
+    '''
     number_of_staff = 0
 
     def __init__(self, name):
--- amity/room.py
+++ amity/room.py
@@ -1,17 +1,33 @@
 class Room(object):
+    ''' Room Class
+
+        This is the Super Class that handles the creation of Rooms
+        in the Amity System. It relies heavily on the information it
+        gets from it's Child Classes.
+    '''
 
     def __init__(self, name, type):
+        self.name = name
         self.total_persons = 0
         self.allocated_persons = []
 
     def add_person(self, uuid):
+        ''' This method is responsible for adding a person to a room. '''
         pass
 
     def remove_person(self, uuid):
+        ''' This method is responsible for removing a person from a room. '''
         pass
 
 
 class Office(Room):
+    ''' Office Class
+
+        This is one of the Child Classes that inherits from the Room
+        Super Class. It passes information to the Parent Constructor
+        so as to dictate how the Room is created. It also handles
+        other activities related to a Office.
+    '''
     number_of_offices = 0
 
     def __init__(self, name):
@@ -20,6 +36,13 @@ class Office(Room):
 
 
 class LivingSpace(Room):
+    ''' LivingSpace Class
+
+        This is one of the Child Classes that inherits from the Room
+        Super Class. It passes information to the Parent Constructor
+        so as to dictate how the Room is created. It also handles
+        other activities related to a Living Space.
+    '''
     number_of_living_spaces = 0
 
     def __init__(self, name):
